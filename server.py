05354cd5144a91aa40416485ca6e6c8d605e4e32
André Fincato <work@andrefincato.info>
2021-05-02 18:06:11 +0200

    setup basic UDP server to listen to MW RC-Feed

--- server.py
+++ server.py
@@ -1,7 +1,13 @@
-import redis
+import socket
 
-r = redis.Redis()
+SERVER_IP = "localhost"
+SERVER_PORT = 1338
 
-for key in r.scan_iter():
-    print('k =>', key)
+server_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+server_sock.bind((SERVER_IP, SERVER_PORT))
 
+print("UDP server has started and is ready to receive")
+
+while True:
+    data, addr = server_sock.recvfrom(2048)
+    print('msg =>', data)
