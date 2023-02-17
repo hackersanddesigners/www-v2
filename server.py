253f56e8534eeb3f025d065cc6e0430f45e7f023
André Fincato <work@andrefincato.info>
2023-02-17 16:30:17 +0100

    server: wrap code inside main func

--- server.py
+++ server.py
@@ -8,37 +8,47 @@ import wikitextparser as wtp
 from jinja2 import Environment, FileSystemLoader
 from fetch import fetch_article
 from parser import parser
+load_dotenv()
 
 
-env = Environment(loader=FileSystemLoader('templates'), autoescape=True)
+def main(SERVER_IP: str, SERVER_PORT: int):
+    env = Environment(loader=FileSystemLoader('templates'), autoescape=True)
 
-SERVER_IP = os.getenv('SERVER_IP')
-SERVER_PORT = int(os.getenv('SERVER_PORT'))
+    server_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    server_sock.bind((SERVER_IP, SERVER_PORT))
 
-server_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-server_sock.bind((SERVER_IP, SERVER_PORT))
+    print("UDP server has started and is ready to receive...",
+          [SERVER_IP, SERVER_PORT])
 
-print("UDP server has started and is ready to receive...")
+    while True:
+        data, addr = server_sock.recvfrom(2048)
+        msg = json.loads(data)
+        print('msg =>', json.dumps(msg, indent=4))
 
-while True:
-    data, addr = server_sock.recvfrom(2048)
-    msg = json.loads(data)
-    print('msg =>', json.dumps(msg, indent=4))
+        # -- we have the UPD message, let's fetch the full article now
+        try:
+            data = fetch_article(msg['title'])
+            article = parser(data)
 
-    #-- we have the UPD message, let's fetch the full article now
-    try:
-        data = fetch_article(msg['title'])
-        article = parser(data)
+            t = env.get_template('article.html')
+            document = t.render(article=article)
 
-        t = env.get_template('article.html')
-        document = t.render(article=article)
+            with open('./wiki/%s.html' % article['slug'], 'w') as f:
+                try:
+                    f.write(document)
+                    print('✓ %s-article "%s" has been correctly written to disk'
+                          % (article['slug'], article['title']))
+                except Exception as e:
+                    print('✕ error for %s-article "%s" =>'
+                          % (article['slug'], article['title']), e)
 
-        with open('./wiki/%s.html' % article['slug'], 'w') as f:
-            try:
-                f.write(document)
-                print('✓ %s-article "%s" has been correctly written to disk' % (article['slug'], article['title']))
-            except Exception as e:
-                print('✕ error for %s-article "%s" =>' % (article['slug'], article['title']), e)
+        except Exception as e:
+            traceback.print_exc()
 
-    except Exception as e:
-        traceback.print_exc()
+
+if __name__ == '__main__':
+
+    SERVER_IP = os.getenv('SERVER_IP')
+    SERVER_PORT = int(os.getenv('SERVER_PORT'))
+
+    main(SERVER_IP, SERVER_PORT)
