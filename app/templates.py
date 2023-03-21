fa8d22d4dd41402fed3d89cf50212d09cd11bd18
André Fincato <work@andrefincato.info>
2023-03-21 19:40:40 +0100

    templates: clean up

--- app/templates.py
+++ app/templates.py
@@ -2,12 +2,12 @@ from jinja2 import Environment, FileSystemLoader
 from slugify import slugify
 from write_to_disk import main as write_to_disk
 
+
 def make_url_slug(url: str):
     return slugify(url)
 
 
 def make_index(articles):
-    print('make-index =>', articles)
     env = Environment(loader=FileSystemLoader('app/templates'), autoescape=True)
     env.filters['slug'] = make_url_slug
     t = env.get_template('index.html')
