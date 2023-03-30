dbef0baf9579830ecd76596442fbe13e66677de4
André Fincato <work@andrefincato.info>
2023-03-30 12:36:56 +0200

    write-to-disk: make func to check for semaphore

--- app/write_to_disk.py
+++ app/write_to_disk.py
@@ -1,10 +1,18 @@
 import aiofiles
 
+async def write(page_slug: str, document: str):
+    async with aiofiles.open(f"./wiki/{page_slug}.html", mode='w') as f:
+        try:
+            await f.write(document)
+            print(f"✓ {page_slug}-article has been correctly written to disk")
+        except Exception as e:
+            print(f"✕ error for {page_slug}-article => {e}")
+
+
 async def main(page_slug: str, document: str, sem):
-    async with sem:
-        async with aiofiles.open(f"./wiki/{page_slug}.html", mode='w') as f:
-            try:
-                await f.write(document)
-                print(f"✓ {page_slug}-article has been correctly written to disk")
-            except Exception as e:
-                print(f"✕ error for {page_slug}-article => {e}")
+    if sem is not None:
+        async with sem:
+            await write(page_slug, document)
+            
+    else:
+        await write(page_slug, document)
