c61176905281ca191a750ea7e129a8518a6321ef
André Fincato <work@andrefincato.info>
2023-04-19 17:46:07 +0200

    main: add comments

--- app/main.py
+++ app/main.py
@@ -13,6 +13,12 @@ app.mount("/",
 app.mount("/assets/media",
           StaticFiles(directory=base_dir / "wiki/assets/media"),
           name="media")
+
+# TODO this route is not working ):
+# we're instead copying CSS and JS files into wiki/assets
+# when running build-wiki for instance
+# maybe it's due to setting `/` with html=True
+# making that path the root path?
 app.mount("/static",
           StaticFiles(directory=base_dir / "static"),
           name="static")
