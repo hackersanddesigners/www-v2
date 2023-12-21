38b1154f4581e2aa8ffbd2a282124e72cf7e63dc
André Fincato <work@andrefincato.info>
2023-12-21 11:32:22 +0100

    update write-to-disk logic

--- app/file_ops.py
+++ app/file_ops.py
@@ -1,7 +1,12 @@
-from os import getenv
+from dotenv import load_dotenv
+import os
+import glob
 from pathlib import Path
 import aiofiles
+load_dotenv()
 
+
+WIKI_DIR = os.getenv('WIKI_DIR')
 
 def file_lookup(filename: str) -> list[str]:
     """
@@ -9,11 +14,9 @@ def file_lookup(filename: str) -> list[str]:
     in WIKI_DIR and return list of results.
     """
 
-    WIKI_DIR = Path(getenv('WIKI_DIR'))
-    
     pattern = f"**/{filename}.html"
     paths = [p for p
-             in WIKI_DIR.glob(pattern)]
+             in Path(WIKI_DIR).glob(pattern)]
 
     return paths
 
@@ -26,20 +29,22 @@ async def write_to_disk(page_slug: str, document: str, sem):
     async def write(page_slug: str, document: str):
         """
         """
-        
-        # let's check if page_slug contains a parent dir (or more) as part of it
-        # if going up one level (parent) and expanding the path (expanduser)
-        # return `.`, there's no dir in the path
-        cat_dir = f"./wiki/{Path(page_slug).parent.expanduser()}"
-        if cat_dir != '.':
-            t = await aiofiles.os.makedirs(cat_dir, exist_ok=True)
 
-            async with aiofiles.open(f"./wiki/{page_slug}.html", mode='w') as f:
-                try:
-                    await f.write(document)
-                    print(f"✓ {page_slug} has been correctly written to disk")
-                except Exception as e:
-                    print(f"✕ error for {page_slug} => {e}")
+        if page_slug is not None:
+        
+            # let's check if page_slug contains a parent dir (or more) as part of it
+            # if going up one level (parent) and expanding the path (expanduser)
+            # return `.`, there's no dir in the path
+            cat_dir = f"./wiki/{Path(page_slug).parent.expanduser()}"
+            if cat_dir != '.':
+                t = await aiofiles.os.makedirs(cat_dir, exist_ok=True)
+
+                async with aiofiles.open(f"./{WIKI_DIR}/{page_slug}.html", mode='w') as f:
+                    try:
+                        await f.write(document)
+                        print(f"✓ {page_slug} has been correctly written to disk")
+                    except Exception as e:
+                        print(f"✕ error for {page_slug} => {e}")
     
 
     if sem is not None:
