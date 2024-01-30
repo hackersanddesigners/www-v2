f49d8421992c293ded83fa7b722f0d1b1b347721
André Fincato <work@andrefincato.info>
2024-01-30 12:43:07 +0100

    live debugging: make-cat-index, pass actual timeout arg

--- app/build_category_index.py
+++ app/build_category_index.py
@@ -72,7 +72,7 @@ async def make_category_index(cat: str):
 
     context = create_context(ENV)
     timeout = httpx.Timeout(10.0, connect=60.0, read=60.0)
-    async with httpx.AsyncClient(verify=context) as client:
+    async with httpx.AsyncClient(verify=context, timeout=timeout) as client:
 
         # -- get full list of entries from category
         data = []
