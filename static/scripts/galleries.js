cd091b330cbbe3c23219a010fd5942a804091ce4
كارل مبارك <bonjour@moubarak.eu>
2024-02-29 12:45:57 +0100

    gallary buttons seems to not work always, add tinmeout cus that might fix it?

--- static/scripts/galleries.js
+++ static/scripts/galleries.js
@@ -1,19 +1,21 @@
 "use strict";
 
 window.addEventListener('load', () => {
+  const controls_template = document.querySelector('#gallery-controls-template')
   for ( const gallery of document.querySelectorAll( '.gallery' ) ) {
-    // const rem = parseFloat(getComputedStyle(document.documentElement).fontSize)
-    const controls = document.querySelector('#gallery-controls-template').content.cloneNode(true)
-    gallery.parentElement.insertBefore( controls, gallery.nextSibling )
-    gallery.nextElementSibling.querySelector( '.right' ).onclick = () => { gallery.scroll({
-      // left: gallery.scrollLeft + gallery.offsetWidth - 10 * rem,
-      left: gallery.scrollLeft + 0.6 * window.innerWidth ,
-      behavior: "smooth"
-    })}
-    gallery.nextElementSibling.querySelector( '.left' ).onclick = () => { gallery.scroll({
-      // left: gallery.scrollLeft - gallery.offsetWidth + 10 * rem,
-      left: gallery.scrollLeft - 0.6 * window.innerWidth,
-      behavior: "smooth"
-    })}
+    const controls = controls_template.content.cloneNode(true)
+    gallery.parentNode.insertBefore( controls, gallery.nextSibling )
+    setTimeout(() => {
+      gallery.nextElementSibling.querySelector( '.right' ).onclick = () => { gallery.scroll({
+        // left: gallery.scrollLeft + gallery.offsetWidth - 10 * rem,
+        left: gallery.scrollLeft + 0.6 * window.innerWidth ,
+        behavior: "smooth"
+      })}
+      gallery.nextElementSibling.querySelector( '.left' ).onclick = () => { gallery.scroll({
+        // left: gallery.scrollLeft - gallery.offsetWidth + 10 * rem,
+        left: gallery.scrollLeft - 0.6 * window.innerWidth,
+        behavior: "smooth"
+      })}
+    }, 300)
   }
 })
